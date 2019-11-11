bcb0f7fbffc877fa9b462e2548d9158c06236725
mia-pan <miapanyuewei@gmail.com>
2019-11-10 18:06:27 -0800

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,30 @@
 class Employee
-
+    @@all = []
+    attr_reader :name, :salary, :manager_name
+    def initialize(name, salary, manager_name)
+        @name = name 
+        @salary = salary
+        @manager_name = manager_name
+        @@all << self 
+    end 
+    def self.all
+        @@all
+    end 
+    def self.paid_over(number)
+        self.all.select do |ep|
+            ep.salary >= number
+        end 
+    end 
+    def self.find_by_department(department_name)
+         m_dp = Manager.all.find do |mg|
+                mg.department == department_name
+                end
+         m_dp.employees[0]
+        
+    end 
+    def  tax_bracket 
+        @@all.select do |ep|
+           @salary-1000 <= ep.salary && ep.salary <= @salary+1000
+        end 
+    end 
 end
